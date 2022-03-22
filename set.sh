3f5d2cadec76be9aeb4350e413fbb502400fdfeb
basanys <bys12ce@gmail.com>
2022-03-23 00:43:15 +0100

    added set to sh

--- set.sh
+++ set.sh
@@ -1,6 +1,6 @@
+set -e
+
 sudo spctl --master-disable
 mv ./OpenHaystackMail.mailbundle ~/Library/Mail/Bundles/
 cd ~/Library/Mail/Bundles/
 codesign --force --deep -s - OpenHaystackMail.mailbundle
-
-
